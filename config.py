52f9e9253ee16c3de7a4d00dd14e52fd26e4050b
Its-kin <133993977+Its-kin@users.noreply.github.com>
2024-03-24 12:29:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority)
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", @Kanha_music_bot)
-PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", @Kings_Queen_BBy)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
@@ -68,7 +68,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 # Get your pyrogram v2 session from @BRANDEDSTRINGSESSION_BOT on Telegram
 STRING1 = getenv("STRING_SESSION", None)
-STRING2 = getenv("STRING_SESSION2", None)
+STRING2 = getenv("STRING_SESSION2", BQDKh88AvJM_Apy9AmwpW7Yt-vWgqJqCN712xW5Yt-fTLe9TOJ5uxko7Z-O6z9_g4W1c_GWpMp6mV2Lw8JM7cZzEbeOSmRc7QkpcsChXKs2R6WHgkOjZqfaskzBqM5Mlyzte0mZM_MQD5CKNL6k5-y_9RwWZLZjKnGQDNOrbf1QpPUs52eQIAIO2DMWSu1oegyS_nD0hud8ZDq2RqB8hxtMCchUH-_3OqqRHitwsHz2HIrX0lFbJn59D37r8eGAU3F_JXSBy5cjf-1XtgoglNZR3GTeqaNMtNR7KzmOVQ9vht7hq0KjImSQTytPX9j9lob9mjMSAfrmKM2LAFuuID33XS54G1AAAAAGFr9cfAA)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
